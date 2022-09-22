5b1ee17108e32a306a3b1f7013654d6a6ed66df0
heheheejin <e_jaxx@hotmail.com>
2022-09-22 21:44:11 +0800

    Create contact serializer and viewset

--- axphone/views.py
+++ axphone/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import ContactSerializer
+from rest_framework import viewsets
+from .models import Contact
 
 # Create your views here.
+
+
+class ContactView(viewsets.ModelViewSet):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
